fddecb0632a2a175a1c0bacc1f1367860177391c
HuyIT3 <hoanghuy010104@gmail.com>
2025-03-14 08:43:46 +0700

    bai tap retrofit

---
+++ app/src/main/java/vn/iotstar/retrofitt7/APIService.java
@@ -0,0 +1,13 @@
+package vn.iotstar.retrofitt7;
+
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.http.GET;
+import retrofit2.Call;
+
+public interface APIService {
+    @GET("categories.php")
+    Call<List<Category>> getCategories();
+
+}
